e90cb51edfc5eb056e54bcd82397977a00b1df47
Nick <Nikolas.amiridis@gmail.com>
2022-11-08 07:35:04 +0200

    Added edit-note page, but not the functionallity yet

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, flash, redirect, url_for
 from flask_login import current_user, login_required
 from .models import Note
 from . import db
@@ -14,17 +14,30 @@ def home():
     new_note = Note(text=note, user_id=current_user.id)
     db.session.add(new_note)
     db.session.commit()
+    return redirect(url_for("views.home"))
 
   return render_template("home.html", user=current_user)
 
 @views.route("/delete-note", methods=["POST"])
 def delete_note():
   note = json.loads(request.data)
-  noteId = note["id"]
-  note_to_delete = Note.query.get(noteId)
+  note_id = note["id"]
+  note_to_delete = Note.query.get(note_id)
   if note:
     if note_to_delete.user_id == current_user.id:
       db.session.delete(note_to_delete)
       db.session.commit()
 
   return jsonify({})
+
+@views.route("/edit-note", methods=["GET", "POST"])
+def edit_note():
+  # if request.method == "POST":
+  #   note = json.loads(request.data)
+    # note_id = note["id"]
+    # note_to_update = Note.query.get(note_id)
+    # if note_to_update.user_id == current_user.id:
+    #   db.session.delete(note_to_update)
+    #   db.session.commit()
+
+  return render_template("edit-note.html", user=current_user)
